2e97da8951d8afe668214ebb30327085b68793b0
Mikhail <logindegtyarev@mail.ru>
2022-01-06 22:40:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/switchtheme/ActivityHome.java
@@ -0,0 +1,57 @@
+package ru.gb.switchtheme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+
+public class ActivityHome extends AppCompatActivity {
+
+    //Variables
+    SwitchMaterial mySwitch;
+    ThemeSharedPref sharedpref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Checking that state true or false from SharedPref
+        sharedpref = new ThemeSharedPref(this);
+
+        setTheme(sharedpref.isNightMode()? R.style.ThemeNight : R.style.ThemeDay);
+        super.onCreate(savedInstanceState);
+
+        //Displaying the layout after setting the theme
+        setContentView(R.layout.activity_home);
+
+        mySwitch= findViewById(R.id.mySwitch);
+        if (sharedpref.isNightMode()) {
+            mySwitch.setChecked(true);
+        }
+
+        //Checking the toggle state and saving it to the SharedPref
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    sharedpref.setNightModeState(true);
+                    restartApp();
+                }
+                else {
+                    sharedpref.setNightModeState(false);
+                    restartApp();
+                }
+            }
+        });
+    }
+
+    //Method to restart the app
+    public void restartApp () {
+        Intent restartApp = new Intent(getApplicationContext(), ActivityHome.class);
+        startActivity(restartApp);
+        finish();
+    }
+}
